9cdf3fba71f632f4cdd45605f7a87e3a0d915202
ChoEunHyun <nyu531@gmail.com>
2016-10-20 09:33:20 +0900

    report card: Get a average of grades

--- report_card.c
+++ report_card.c
@@ -3,19 +3,19 @@
 int main()
 {
 	int kor, eng, math;
-	int sum = 0;
+	int average = 0;
 
 	math = 80;
 	eng = 100;
 	kor = 90;
-	sum = 80 + 100 + 90;
+	average = 80 + 100 + 90;
 
 	printf("This program print report card.\n");
 
 	printf("Korean : %d\n", kor);
 	printf("English : %d\n", eng);
 	printf("Math : %d\n", math);
-	printf("Sum : %d\n", sum);
-	printf("Mean : %d\n", sum/3);
+	printf("Sum : %d\n", average);
+	printf("Mean : %d\n", average/3);
 	return 0;
 }
